8809a0f457a44c98efb0e2b0c2bef6301d6e6350
ray <ray@hotmail.com>
2018-10-24 08:29:50 -0500

    Se agregaron los arechivos al master

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Práctica usando git</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/estilo.css" />
+   
+</head>
+<body>
+    <div class="clase1">
+
+    </div>
+</body>
+</html>
